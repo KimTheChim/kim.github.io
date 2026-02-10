1910045312e79faeec4c009fbfdfc8fa6ad30a18
KimTheChim <kagram123@gmail.com>
2026-02-10 23:25:05 +0100

    This is an old project i want to publish

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-white font-semibold">
+  <div class="w-vw flex h-20 items-center gap-4 mr-40">
+    <div class="flex-1 ml-30 py-2 z-10">KIM AG</div>
+    <div class="flex space-x-20 -mr-20">
+      <div class="z-10 flex h-10 min-w-20 justify-center p-2 border border-transparent hover:border-white transition"><p class="cursor-pointer">About Me</p></div>
+      <div class="z-10 flex h-10 min-w-20 justify-center p-2 border border-transparent hover:border-white transition"><p class="cursor-pointer">Projects</p></div>
+      <div class="z-10 flex h-10 min-w-20 justify-center p-2 border border-transparent hover:border-white transition"><p class="cursor-pointer">Contact Me</p></div>
+    </div>
+  </div>
+</div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="fadein min-w-100 max-h-screen flex space-x-20 items-top justify-center opacity-100 2xl:ml-[10%]">
+    <div class="ml-[5%] floating filter blur-3xl min-w-[30vw] min-h-100 bg-red-500 absolute -z-10 -rotate-15 mix-blend-multiply left-20 top-30 2xl:ml-[10%]"></div>
+    <div class="ml-[5%] grow filter blur-3xl min-w-[30vw] min-h-100 bg-pink-500 absolute -z-10 -rotate-35  mix-blend-multiply left-40 top-30 2xl:ml-[10%]"></div>
+    <div class="ml-[5%] grow filter blur-3xl min-w-[30vw] min-h-100 bg-red-500 absolute -z-10 -rotate-65  mix-blend-multiply left-80 top-30 2xl:ml-[10%]"></div>
+    <h1 class="font-bold text-9xl mt-50"><p class="font-light text-6xl ml-36">KIM ANDRÉ</p><p class="ml-32">GRAMSTAD</p></h1>
+    <div class="floating pb-3 flex flex-col basis-0 min-w-[10vw] justify-end mr-[5%]">Aspiring <p class="bg-gradient-to-r from-red-500 to-white text-transparent bg-clip-text"> Front-End </p> developer with  a <br> ❤️ for great <p class="bg-gradient-to-r from-red-500 to-white text-transparent bg-clip-text"> UI & Design </p> </div>
+  </div>
+
+<!-- class="bg-gradient-to-r from-red-500 to-white text-transparent bg-clip-text" -->
+</template>
